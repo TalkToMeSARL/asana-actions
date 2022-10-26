690c2e3706a3c9ff8ddf23cb081ba95a26b2cc21
Bryan Coupy <bryan@talk-to-me.ch>
2022-10-26 16:20:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+# link-asana-commit-gh-action
+
+We often want to submit code that refers to tasks in Asana.
+
+This action links a Github commit with an Asana task as provided by the commit's message. If users provide an Asana task URL as described below, this action will post a comment on the given task with the URL of the git commit.
+
+## Inputs
+
+## `asana-pat`
+
+**Required** The Asana Personal Access Token (PAT). See https://asana.com/guide/help/api/api.
+
+## Example usage
+
+Setup a Github Action:
+
+```
+name: LinkComitToAsanaTask
+
+on:
+  push:
+    branches: [ main ]
+
+jobs:
+  linkAsana:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout
+      uses: actions/checkout@v3
+    - name: Link to Asana
+      uses: eithanshavit/link-asana-commit-gh-action@v2
+      with:
+        asana-pat: <Your Asana Private Token>
+
+```
+
+Then, commit with a message that contains the text:
+
+```
+TASK: <Your Asana task URL>
+```
---
+++ index.js
@@ -0,0 +1,97 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const asana = require("asana");
+
+async function writeComment(asanaClient, taskId, comment) {
+  try {
+    await asanaClient.tasks.findById(taskId);
+  } catch (error) {
+    core.setFailed("Asana task not found: " + error.message);
+    return;
+  }
+
+  try {
+    await asanaClient.tasks.addComment(taskId, {
+      text: comment,
+    });
+    core.info(`Added the commit link the Asana task ${taskId}.`);
+  } catch (error) {
+    core.setFailed("Unable to add comment to task");
+    return;
+  }
+}
+
+function extractTaskID(commitMessage) {
+  const regex = new RegExp(
+    "TASK.+(https:\\/\\/app\\.asana\\.com\\/(\\d+)\\/(?<project>\\d+)\\/(?<task>\\d+).*?)",
+    "gm"
+  );
+
+  let m;
+
+  while ((m = regex.exec(commitMessage)) !== null) {
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    if (m.groups.task) {
+      return m.groups.task;
+    }
+  }
+  return null;
+}
+
+async function processCommit(asanaClient, commit) {
+  core.info("Processing commit " + commit.url);
+  const taskId = extractTaskID(commit.message);
+  if (taskId) {
+    writeComment(asanaClient, taskId, commit.message + "\nRef: " + commit.url);
+  } else {
+    core.notice(`No Asana task URL provided in commit message.`);
+  }
+}
+
+async function main() {
+  if (!process.env.TEST && github.context.eventName !== "push") {
+    core.setFailed(
+      "Action must be triggered with push event. It is " +
+        github.context.eventName
+    );
+    return;
+  }
+  const pushPayload = github.context.payload;
+
+  let payloadCommits = pushPayload.commits;
+  const commits =
+    process.env.COMMITS != null
+      ? JSON.parse(process.env.COMMITS)
+      : payloadCommits;
+  if (!Array.isArray(commits) || !commits.length) {
+    core.setFailed("Unable to read commits from event");
+    return;
+  }
+
+  const asanaPAT = process.env.ASANAPAT || core.getInput("asana-pat");
+  if (!asanaPAT) {
+    core.setFailed("Asana access token not found!");
+    return;
+  }
+
+  const asanaClient = asana.Client.create({
+    defaultHeaders: { "asana-enable": "new-sections,string_ids" },
+    logAsanaChangeWarnings: false,
+  }).useAccessToken(asanaPAT);
+  if (!asanaClient) {
+    core.setFailed("Unable to establish an Asana API client");
+    return;
+  }
+
+  for (const commit of commits) {
+    processCommit(asanaClient, commit);
+  }
+}
+
+try {
+  main();
+} catch (error) {
+  core.setFailed(error.message);
+}
